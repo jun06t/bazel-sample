7795093686a73c4c439ed249eb23e14f890f0c47
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2022-03-02 05:22:15 +0900

    feat: support git tags match

--- ldflags/tag.sh
+++ ldflags/tag.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo GIT_COMMIT $(git rev-parse --verify HEAD)
+if [ "${IMAGE_TAG}" = "latest" ]; then
+    BUILD_VERSION_PREFIX=`echo ${BUILDABLE}`
+    echo GIT_COMMIT $(git describe --tags --match ${BUILD_VERSION_PREFIX}/*)
+else
+    echo GIT_COMMIT $(git describe --tags)
+fi
 echo DATE $(date '+%Y/%m/%d %H:%M:%S %Z')
