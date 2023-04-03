55563b144e1dcd0ad48d3593386d3140577bcc23
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2023-04-03 19:19:47 +0900

    feat: add data property

--- test/dice/dice_test.go
+++ test/dice/dice_test.go
@@ -1,6 +1,8 @@
 package dice
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,4 +15,14 @@ func TestPlayDice(t *testing.T) {
 		assert.GreaterOrEqual(t, out, 1)
 		assert.LessOrEqual(t, out, 6)
 	}
+}
+
+func TestExternalFile(t *testing.T) {
+	b1, err := os.ReadFile("./test1.yaml")
+	assert.NoError(t, err)
+	log.Println(string(b1))
+
+	b2, err := os.ReadFile("./testdata/test2.yaml")
+	assert.NoError(t, err)
+	log.Println(string(b2))
 }
